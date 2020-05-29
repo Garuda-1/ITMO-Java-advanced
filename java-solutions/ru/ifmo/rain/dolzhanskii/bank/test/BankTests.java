1a489ac63e73c3696a9ad7cf344514be724d2954
Ian <yan.dolganskiy@mail.ru>
2020-05-29 11:11:27 +0300

    HW11: Minor fix

--- java-solutions/ru/ifmo/rain/dolzhanskii/bank/test/BankTests.java
+++ java-solutions/ru/ifmo/rain/dolzhanskii/bank/test/BankTests.java
@@ -25,6 +25,7 @@ public class BankTests {
         launcher.registerTestExecutionListeners(listener);
         launcher.execute(request);
         TestExecutionSummary summary = listener.getSummary();
+
         summary.printTo(new PrintWriter(new OutputStreamWriter(System.out)));
         System.exit(summary.getFailures().size());
     }
